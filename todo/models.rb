5b457286db2720f91f581dc14b9ffb10884a601c
haru <harutsuka0427@gmail.com>
2024-05-31 18:42:45 +0900

    add: nameとpasswordにバリデーションを追加

--- todo/models.rb
+++ todo/models.rb
@@ -5,4 +5,6 @@ ActiveRecord::Base.establish_connection
 
 class User < ActiveRecord::Base
     has_secure_password
+    validates :name, presence: true,format: { with: /\A\w+\z/ }
+    validates :password, length: { in: 5..10 }
 end
